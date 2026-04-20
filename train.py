bfe91c20e42a307de3d868bb8726549e83a95c37
Dmytrii Puzyr <slymahel@gmail.com>
2026-04-20 19:03:00 +0000

    Exp14: asymmetric aug + batch=256 - combine Exp13 sensor gap aug with harder in-batch negatives

--- train.py
+++ train.py
@@ -84,7 +84,7 @@ class Config:
     image_size: int = 336
     embedding_dim: int = 768  # CLS token dim, no projection head
 
-    batch_size: int = 128
+    batch_size: int = 256
     eval_batch_size: int = 128
     num_workers: int = 8
 
